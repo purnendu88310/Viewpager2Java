04f2bbf082c2881091e376e5a5ad0cb5df3a1784
rishabhshukla31 <rishabh.shukla@mastek.com>
2024-01-11 15:50:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/StudentModel.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+public class StudentModel {
+
+    private String name;
+    private String subject;
+
+    public StudentModel(String name, String subject) {
+
+        this.name = name;
+        this.subject = subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
